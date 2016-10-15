60da6457a05e6fb8a4744c107b755ab6a115a3a2
Brunoxd <bruno.mendes.costa@usp.br>
2016-10-15 11:24:35 -0300

    Estimates the time to copy data.

---
+++ Project_9/cp.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+/* Função que estima o tempo restante. */
+int evaluate(int total, int progress) {
+	if(progress != 0) 		
+		printf("Time remaining: %d seconds.\n", (int) ((5 * total)/progress - 0.5));
+	 else
+		/* Caso o número de bytes acumulado seja 0. */
+		printf("Time remaining: stalled\n");	
+	return 0;
+}
+
+int main(int argc, char const *argv[]) {
+	int total = 0, progress = 0, bytes = 0, i = 0;	
+
+	/* Realiza a leitura do total de bytes a ser transferido. */
+	scanf("%d", &total);
+
+	/* Imprime o total de bytes. */
+	printf("Data size of %d bytes:\n", total);
+
+	for(i = 1; total > 0; i++) {
+		/* Realiza a leitura do número de bytes lido. */
+		scanf("%d", &bytes);
+
+		/* Incrementa o progresso de cópia. */
+		progress += bytes;
+
+		/* Atualiza o restante. */
+		total -= bytes;			
+
+		/* A cada 5 segundos, realiza a estimativa de tempo. */
+		if(i % 5 == 0) progress = evaluate(total, progress);		
+	}
+
+	printf("Total time: %d seconds\n", i-1);
+
+	return 0;
+}
